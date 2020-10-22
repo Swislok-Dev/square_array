0dc32e2cd81f1eae3fa528a5d7f0bc6ef6eb6c1b
cmarker <cmarker4@gmail.com>
2020-10-21 21:53:12 -0500

    Done.

--- square_array.rb
+++ square_array.rb
@@ -4,5 +4,11 @@ def square_array(array)
     arr << num ** 2
   end
   arr
-
 end
+
+# The following provides the same as above. 
+# For reference only.
+
+# def square_array(array)
+#   array.each_with_object([]) { |i, arr| arr << i ** 2 }
+# end
